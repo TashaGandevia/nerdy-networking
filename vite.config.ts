f450c531a380c4ac914e884f9cf89b597d6ef6d9
Tasha Gandevia <100049187+TashaGandevia@users.noreply.github.com>
2026-06-24 07:48:02 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/nerdy-networking/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
